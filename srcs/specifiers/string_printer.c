f19d5cf21e5c71f01a85a36b33d4df314b33ae0b
Dyamen1411 <massias.antoine@gmail.com>
2023-10-23 01:45:27 +0200

    Fix: When the `NULL` is passed as an argument to `\%s` and the precision field is set to a value less than `7`, a part of the string `(null)` was displayed where as nothing should have been printed.

--- srcs/specifiers/string_printer.c
+++ srcs/specifiers/string_printer.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   _string_printer.c                                  :+:      :+:    :+:   */
+/*   string_printer.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 03:58:39 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/20 04:26:28 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/23 01:40:35 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
+
+#define NULL_STRING "(null)"
 
 int	_string_printer(va_list *list, t_format *fmt)
 {
 	char	*str;
 
 	str = va_arg(*list, char *);
-	if (str == NULL)
-		str = (char *) "(null)";
-	return (string_printer(fmt, str));
+	if (str)
+		return (string_printer(fmt, str));
+	if (!fmt__precision(fmt)
+		|| (unsigned long) fmt->precision + 1 >= sizeof(NULL_STRING))
+		return (string_printer(fmt, (char *) NULL_STRING));
+	return (string_printer(fmt, (char *) ""));
 }
